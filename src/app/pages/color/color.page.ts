7abcadce70fd3acd3e91ceb6d5e5775cda0150d5
Sergio Enjuanes <srgenjuanes@gmail.com>
2022-08-21 01:59:00 +0200

    Color 2.1

--- src/app/pages/color/color.page.ts
+++ src/app/pages/color/color.page.ts
@@ -18,6 +18,7 @@ export class ColorPage implements OnInit, OnDestroy {
 
   colorPickerOpen = false;
   color = TORONJA_COLOR;
+  isLight = true;
 
   layoutVisible = false;
   isFullscreen = false;
@@ -47,6 +48,7 @@ export class ColorPage implements OnInit, OnDestroy {
     this.subscriptionUrlChange = this.route.params.subscribe((params) => {
       this.color = params.color;
       if (this.color) {
+        this.setIsLight();
         localStorage.setItem('favoriteColor', this.color);
         this.utilsService.changeThemeColor(this.color);
       } else {
@@ -62,6 +64,16 @@ export class ColorPage implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.subscriptionUrlChange.unsubscribe();
     this.subjectHideLayout.unsubscribe();
+  }
+
+  setIsLight() {
+    if (typeof this.color === 'string' && this.color.length === 6) {
+      const red = parseInt(this.color.substring(0, 2), 16);
+      const green = parseInt(this.color.substring(2, 4), 16);
+      const blue = parseInt(this.color.substring(4, 6), 16);
+
+      this.isLight = ((red + green + blue) / 3) > 127.5;
+    }
   }
 
   async openColorPicker() {

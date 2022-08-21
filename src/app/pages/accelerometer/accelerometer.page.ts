2222106b46d97b5f16b4d667b1c81f468405311d
Sergio Enjuanes <srgenjuanes@gmail.com>
2022-08-21 04:53:30 +0200

    Fix

--- src/app/pages/accelerometer/accelerometer.page.ts
+++ src/app/pages/accelerometer/accelerometer.page.ts
@@ -25,9 +25,9 @@ export class AccelerometerPage implements OnInit, OnDestroy {
       this.acl = new Accelerometer({frequency: 60});
       this.acl.addEventListener('reading', () => {
         this.accelerometerIsRead = true;
-        this.x = this.acl.x;
-        this.y = this.acl.y;
-        this.z = this.acl.z;
+        this.x = Math.round(this.acl.x * 100) / 100;
+        this.y = Math.round(this.acl.y * 100) / 100;
+        this.z = Math.round(this.acl.z * 100) / 100;
       });
 
       this.acl.start();
